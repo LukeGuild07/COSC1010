fd23095b02357f1004245b915c8ebff518f6c583
Luke <lukeguild@westernwyoming.edu>
2024-09-05 15:50:07 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #
 # Name Luke Guild
-# Date 09/02/2024
-# Your First Python Program
-# COSC 1010 
+# Date 9/4/2024
+# Sales Prediction Programming Project
+# COSC 2409 DNT
 #
 
-# This is my first Python program.
-print("Hello World!")
+# Variables to hold the sales total and the profit
+total_sales=float(input('Enter the projected sales: '))
+
+# Get the amount of projected sales.
+
+# Calculate the projected profit.
+profit=total_sales*0.23
+# Print the projected profit.
+print('The projected profit is $',profit)
