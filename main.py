8ffc34d7a030c55faebcc204f9ed00b2baff99a2
Luke <lukeguild@westernwyoming.edu>
2024-09-08 12:12:49 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 #
 # Name Luke Guild
-# Date 9/8/2024
-# Sales Tax Programming Project
+# Date 9/4/2024
+# Sales Prediction Programming Project
 # COSC 2409 DNT
 #
 
-# Variable declarations
-state_tax = 0.05
-county_tax = 0.025
-# Constants for the state and county tax rates
+# Variables to hold the sales total and the profit
+total_sales=float(input('Enter the projected sales: '))
 
-# Get the amount of the purchase.
-purchase_amount = int(input('Enter purchase amount: '))
-# Calculate the state sales tax.
-total_state_tax = state_tax * purchase_amount
-# Calculate the county sales tax.
-total_county_tax = county_tax * purchase_amount
-# Calculate the total tax.
-total_tax = total_state_tax + total_county_tax
-# Calculate the total of the sale.
-total_sale_cost = total_tax + purchase_amount
-# Print information about the sale.
-print(f'The amount of purchase is ${purchase_amount:.2f}.')
-print(f'The state tax is ${total_state_tax:.2f}.')
-print(f'The county tax is ${total_county_tax:.2f}.')
-print(f'The total tax is ${total_tax:.2f}.')
-print(f'The total cost of purchase is ${total_sale_cost:.2f}.')
+# Get the amount of projected sales.
+
+# Calculate the projected profit.
+profit=total_sales*0.23
+# Print the projected profit.
+print('The projected profit is $',profit)
