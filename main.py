b01035ec4a963719dc789f90f670f685be707891
Luke <lukeguild@westernwyoming.edu>
2024-09-14 15:21:33 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 #
 # Name Luke Guild
 # Date 9/14/2024
-# Areas of Rectangles Programming Project
+# Hot Dog Cookout Calculator Programming Project
 # COSC 2409 DNT
 #
-# Local variables
-
-# Get length A
-length_A= int(input('Enter the length of rectangle A: '))
-# Get width A
-width_A= int(input('Enter the width of rectangle A: '))
-# Get length B
-length_B= int(input('Enter the length of rectangle B: '))
-# Get width B
-width_B= int(input('Enter the width of rectangle B: '))
-# Calculate area A
-area_A= length_A*width_A
-# Calculate area B
-area_B= length_B*width_B
-# Print area comparison using if-elif-else statements
-if area_A < area_B:
-    print('Rectangle B has a larger area')
-elif area_B < area_A:
-    print('Rectangle A has a larger area')
-else:
-    print('Both rectangles have an equal area')
+# Use comments liberally throughout the program. 
+# Named Constants
+HOTDOG_PACK=10
+HOTDOG_BUNS=8
+# User input
+people= int(input('How many people are going to be at the cookout?: '))
+hotdogs_per_person= int(input('How many hotdogs does each person get?'))
+# Calculate total hotdogs/buns required
+total_hotdogs= people*hotdogs_per_person
+# Calculate number of hotdog packages required
+hotdog_packages= total_hotdogs/HOTDOG_PACK
+# Calculate number of bun packages required
+hotdog_bun_packs= total_hotdogs/HOTDOG_BUNS
+# Some math stuff
+import math
+total_hotdog_packages=(math.ceil(hotdog_packages))
+total_bun_packages=(math.ceil(hotdog_bun_packs))
+# Calculate number of hotdogs/buns leftover
+hotdog_decimal= total_hotdog_packages-hotdog_packages
+leftover_hotdogs= hotdog_decimal*HOTDOG_PACK
+bun_decimal= total_bun_packages-hotdog_bun_packs
+leftover_buns= bun_decimal*HOTDOG_BUNS
+# Display everything
+print('You need at least',total_hotdog_packages,'hotdog packages')
+print('You need at least',total_bun_packages,'bun packages')
+print('You will have',leftover_hotdogs,'hotdogs left over')
+print('You will have',leftover_buns,'buns left over')
