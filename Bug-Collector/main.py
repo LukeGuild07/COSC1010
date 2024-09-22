6c30d3bee98a53c628b6399e3f05f74d35b9bf8b
Luke <lukeguild@westernwyoming.edu>
2024-09-22 08:51:51 -0600

    Add files via upload

--- Bug-Collector/main.py
+++ Bug-Collector/main.py
@@ -1,11 +1,15 @@
 #
-# Name
-# Date
+# Name Luke Guild
+# Date 9/22/2024
 # Bug Collector Programming Project
 # COSC 2409 DNT
 #
 # Initialize variables for bugs and total number of bugs collected.
-
+total_bugs=0
 # Get number of bugs collected each day using a for loop.
-
+for day in range(1, 6):
+    print('Enter the number of bugs collected on day', day)
+    bugs= int(input())
+    total_bugs= total_bugs+ bugs
 # Display the total number of bugs collected.
+print('You have collected a total of',total_bugs,'bugs')
