1114320be0cc50ee31479a5a5f50dfd9c4bbbf2e
Luke <lukeguild@westernwyoming.edu>
2024-09-29 13:31:04 -0600

    Add files via upload

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -1,7 +1,13 @@
 #
-# Name
-# Date
+# Name Luke Guild
+# Date 9/29/2024
 # Feet to Inches Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program.
+# Ask user for feet
+feet=int(input('Enter Feet: '))
+# Calculate inches
+inches=feet*12
+# Display Inches
+print('You have', inches ,'inches')
