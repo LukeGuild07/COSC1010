7325ba28ded04f703a14becdf47200982feed48f
Luke <lukeguild@westernwyoming.edu>
2024-09-29 13:23:12 -0600

    Add files via upload

--- Kilometer-Converter/kilometer_converter.py
+++ Kilometer-Converter/kilometer_converter.py
@@ -1,7 +1,13 @@
 #
-# Name
-# Date
+# Name Luke Guild
+# Date 9/29/2024
 # Kilometer Converter Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+# Ask user for kilometers
+kilometers=float(input('Enter kilometers: '))
+# Convert into miles
+miles=kilometers*0.6214
+# Display Miles
+print('Miles Conversion', miles)
