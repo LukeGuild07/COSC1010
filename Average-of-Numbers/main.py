ef7f85f3e363c3701ce96f4233dc408b187ffc56
Luke <lukeguild@westernwyoming.edu>
2024-10-20 09:24:47 -0600

    Add files via upload

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,24 @@
 #
-# Name
-# Date
+# Name Luke Guild
+# Date 10/20/2024
 # Average of Numbers Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+# Initaliaze variables
+total = 0
+count = 0
+# Open the file
+with open('numbers.txt', 'r') as file:
+# Read the lines in the file
+    for line in file:
+# Change line into an integer and add it to the total
+        number = int(line.strip())
+        total += number
+        count += 1
+# Figure out average
+if count > 0:
+    average = total/count
+    print(f"The average of the numbers in the file is: {average}")
+else:
+    print("There are no numbers in the file.")
--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,13 @@
 #
-# Name
-# Date
+# Name Luke Guild
+# Date 10/20/2024
 # File Display Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+# Open he file
+with open('number.txt','r') as file:
+# Examine each line
+    for line in file:
+# Get rid of whitespace and display
+        print(line.strip())
