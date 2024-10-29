a948bda062a84bded18cde5820a39e41d4fdee32
Luke <lukeguild@westernwyoming.edu>
2024-10-28 18:49:39 -0600

    Add files via upload

--- Exception-Handling/main.py
+++ Exception-Handling/main.py
@@ -1,7 +1,31 @@
 #
-# Name
-# Date
+# Name Luke Guild
+# Date 10/28/2024
 # Exception Handling Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+def calculate_average (filename):
+    try:
+        with open(filename,'r') as file:
+            numbers = file.readlines()
+# Convert Lines to Integers
+        integers = []
+        for num in numbers:
+            try:
+                stripped_num = num.strip()
+# Check For Non-Empty Line
+                if stripped_num:
+                    integers.append(int(stripped_num))
+            except ValueError:
+                print(f"The line '{num.strip()}'is not a valid integer")
+        if not integers:
+            raise ZeroDivisionError ("No valid numbers")
+    except IOError as io_error:
+        print(f"An I/O arror occured")
+    except ZeroDivisionError as e:
+        print(f"{e}")
+    except Exception as e:
+        print(f"An unexpected error occured: {e}")
+# Call The Function With the Filename
+calculate_average('numbers.txt')
--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,23 @@
 #
-# Name
-# Date
+# Name Luke Guild
+# Date 10/28/2024
 # Lottery Number Generator Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+# Import The Random
+import random
+def generate_lottery_number():
+# Generate The List
+    lottery_number = [random.randint(0,9) for _ in range (7)]
+    return lottery_number
+def display_lottery_number(lottery_number):
+# Display The List
+    print ("Your lottery number is:")
+    for digit in lottery_number:
+        print (digit, end='')
+# Use a Newline For Better Formattng
+    print()
+# Generate and Display
+lottery_number = generate_lottery_number()
+display_lottery_number(lottery_number)
